b7e2cfc953ee7ec3b0b83ba56ebdb4ed6484ca31
latticetower <merlettaia@gmail.com>
2020-10-07 14:30:35 +0300

    fix bugs

--- data/scripts/labelme2detectron.py
+++ data/scripts/labelme2detectron.py
@@ -1,3 +1,10 @@
+"""
+Sample call:
+
+python scripts/labelme2detectron.py alpacas --save_path annotations/validation.json --dataset_name validation --csv processing-notebooks/final.csv  --masks_dir raw-openimages/annotations/correct-masks/
+
+"""
+
 import click
 import json
 import pandas as pd
@@ -35,11 +42,11 @@ def process_shapes(df, image_path, shapes, height, width, masks_dir):
             b, v = v, b
 
         mask_rectangles[i] = ((a, b), (u, v))
-
+    if not (df.ImageID == image_id).any():
+        return
     for mask_path, dataset_name in df.loc[df.ImageID == image_id, ["MaskPath", "dataset"]].values:
         segmentation_mask = cv2.imread(os.path.join(masks_dir, mask_path), 0)
-        if segmentation_mask.shape!= (height, width):
-
+        if segmentation_mask.shape != (height, width):
             segmentation_mask = cv2.resize(segmentation_mask, (width, height)) > 0
 
         for i, ((x,y), (u, v)) in mask_rectangles.items():
@@ -77,7 +84,8 @@ def process_shapes(df, image_path, shapes, height, width, masks_dir):
     help="file with collected annotations from OpenImages"
 )
 @click.option('--save_path', default=None)
-def process_files(src, masks_dir, csv, save_path):
+@click.option('--dataset_name', default=None)
+def process_files(src, masks_dir, csv, save_path, dataset_name="train"):
     """Print FILENAME."""
     json_paths = [
         os.path.join(src, x)
@@ -86,7 +94,7 @@ def process_files(src, masks_dir, csv, save_path):
     ]
 
     df = pd.read_csv(csv)
-    df = df[df.LabelName=="/m/0pcr"]
+    df = df[(df.LabelName == "/m/0pcr") & (df.dataset == dataset_name)]
     all_shapes = []
     for path in tqdm(json_paths):
         image_path, height, width, shapes = read_labelme_annotation(path)
@@ -98,12 +106,14 @@ def process_files(src, masks_dir, csv, save_path):
         }
         for x in process_shapes(df, image_path, shapes, height, width, masks_dir):
             prepared['annotations'].append(x)
+        if len(prepared['annotations']) > 0:
             all_shapes.append(prepared)
     if save_path is None:
         print(all_shapes)
     else:
         with open(save_path, "w") as f:
             json.dump(all_shapes, f)
+    print(len(all_shapes))
 
 
 if __name__ == '__main__':
